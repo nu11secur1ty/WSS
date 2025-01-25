03e611c94736d282230383947d57dbe5975bafe5
nu11secur1ty <venvaropt@gmail.com>
2025-01-25 07:51:18 +0200

    Up-fingerprint
    Up-fingerprint

--- modules/fingerprint/cms.py
+++ modules/fingerprint/cms.py
@@ -11,7 +11,8 @@ def wordpress(headers,content):
 	_cms_ |= search(decode('<meta name="generator" content="WordPress'),content) is not None
 	_cms_ |= search(decode('<a href="http://www.wordpress.com">Powered by WordPress</a>'),content) is not None
 	_cms_ |= search(decode('<link rel=\'https://api.w.org/\''),content) is not None
-	_cms_ |= search(decode('\\?\/wp-content\\?\/plugins\/|\\?\/wp-admin\\?\/admin-ajax.php'),content) is not None
+	# decode utf8
+	_cms_ |= search(decode('\x5c\x5c\x3f\x5c\x2f\x77\x70\x2d\x63\x6f\x6e\x74\x65\x6e\x74\x5c\x5c\x3f\x5c\x2f\x70\x6c\x75\x67\x69\x6e\x73\x5c\x2f\x7c\x5c\x5c\x3f\x5c\x2f\x77\x70\x2d\x61\x64\x6d\x69\x6e\x5c\x5c\x3f\x5c\x2f\x61\x64\x6d\x69\x6e\x2d\x61\x6a\x61\x78\x2e\x70\x68\x70'),content) is not None
 	if _cms_:
 		return "wordpress"
 
@@ -20,7 +21,8 @@ def joomla(headers,content):
 	if 'Set-Cookie' in headers.keys():
 		_cms_ |= search("mosvisitor=",headers["Set-Cookie"],I) is not None
 	_cms_ |= search(decode("<meta name=\"Generator\" content=\"Joomla! - Copyright (C) 200[0-9] - 200[0-9] Open Source Matters. All rights reserved.\" />"),content) is not None
-	_cms_ |= search(decode("<meta name=\"generator\" content=\"Joomla! (\d\.\d) - Open Source Content Management\" />"),content) is not None
+	# decode utf8
+	_cms_ |= search(decode("\x3c\x6d\x65\x74\x61\x20\x6e\x61\x6d\x65\x3d\x5c\x22\x67\x65\x6e\x65\x72\x61\x74\x6f\x72\x5c\x22\x20\x63\x6f\x6e\x74\x65\x6e\x74\x3d\x5c\x22\x4a\x6f\x6f\x6d\x6c\x61\x21\x20\x28\x5c\x64\x5c\x2e\x5c\x64\x29\x20\x2d\x20\x4f\x70\x65\x6e\x20\x53\x6f\x75\x72\x63\x65\x20\x43\x6f\x6e\x74\x65\x6e\x74\x20\x4d\x61\x6e\x61\x67\x65\x6d\x65\x6e\x74\x5c\x22\x20\x2f\x3e"),content) is not None
 	_cms_ |= search(decode("Powered by <a href=\"http://www.joomla.org\">Joomla!</a>."),content) is not None
 	if _cms_ :
 		return "joomla"
@@ -33,8 +35,9 @@ def drupal(headers,content):
 	_cms_ |= search(decode("<script type=\"text/javascript\" src=\"[^\"]*/misc/drupal.js[^\"]*\"></script>"),content) is not None
 	_cms_ |= search(decode("<[^>]+alt=\"Powered by Drupal, an open source content management system\""),content) is not None
 	_cms_ |= search(decode("@import \"[^\"]*/misc/drupal.css\""),content) is not None
-	_cms_ |= search(decode("jQuery.extend\(drupal\.S*"),content) is not None
-	_cms_ |= search(decode("Drupal.extend\(\S*"),content) is not None
+	# decode utf8
+	_cms_ |= search(decode("\x6a\x51\x75\x65\x72\x79\x2e\x65\x78\x74\x65\x6e\x64\x5c\x28\x64\x72\x75\x70\x61\x6c\x5c\x2e\x53\x2a"),content) is not None
+	_cms_ |= search(decode("\x44\x72\x75\x70\x61\x6c\x2e\x65\x78\x74\x65\x6e\x64\x5c\x28\x5c\x53\x2a"),content) is not None
 	if _cms_ :
 		return "drupal"
 
